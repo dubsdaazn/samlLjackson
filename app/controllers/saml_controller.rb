d8f00d8858402f4214d8f3205a41a8ef7f3ac52f
Jared Branum <jared.branum@gmail.com>
2012-12-03 14:27:30 -0500

    changed saml_settings to reflect url/host of the box it's on

--- app/controllers/saml_controller.rb
+++ app/controllers/saml_controller.rb
@@ -1,6 +1,7 @@
 class SamlController < ApplicationController
     def init
       request = Onelogin::Saml::Authrequest.new
+      raise saml_settings.issuer.inspect
       redirect_to(request.create(saml_settings))
     end
 
@@ -21,8 +22,8 @@ class SamlController < ApplicationController
     def saml_settings
       settings = Onelogin::Saml::Settings.new
 
-      settings.assertion_consumer_service_url = "http://24.112.77.250/saml/consume"
-      settings.issuer                         = "http://24.112.77.250"
+      settings.assertion_consumer_service_url = saml_consume_url(host: request.host)
+      settings.issuer                         = "http://#{request.port == 80 ? request.host : request.host_with_port}"
       settings.idp_sso_target_url             = "https://jaredbranum.okta.com/home/template_saml_2_0/0oa1ny13dcLZNMYMERHB/3079"#{}"https://app.onelogin.com/saml/signon/#{OneLoginAppId}"
       settings.idp_cert_fingerprint           = "CD:C7:C0:8A:DD:0E:0E:94:B1:33:0A:CA:EC:08:29:CC:44:85:A8:23"
       settings.name_identifier_format         = "urn:oasis:names:tc:SAML:2.0:attrname-format:unspecified"
