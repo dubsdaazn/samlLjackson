75b185db9d7bfcfdab0608842d4dbeb0d0e43a61
Jared Branum <jared.branum@gmail.com>
2012-12-03 15:51:57 -0500

    changed saml_settings to use primedia.okta keys instead of jaredbranum.okta keys

--- app/controllers/saml_controller.rb
+++ app/controllers/saml_controller.rb
@@ -23,8 +23,8 @@ class SamlController < ApplicationController
 
       settings.assertion_consumer_service_url = saml_consume_url(host: request.host)
       settings.issuer                         = "http://#{request.port == 80 ? request.host : request.host_with_port}"
-      settings.idp_sso_target_url             = "https://jaredbranum.okta.com/home/template_saml_2_0/0oa1ny13dcLZNMYMERHB/3079"#{}"https://app.onelogin.com/saml/signon/#{OneLoginAppId}"
-      settings.idp_cert_fingerprint           = "CD:C7:C0:8A:DD:0E:0E:94:B1:33:0A:CA:EC:08:29:CC:44:85:A8:23"
+      settings.idp_sso_target_url             = "https://primedia.okta.com/app/template_saml/k18eco74OXEHHWDWRDMZ/sso/saml"
+      settings.idp_cert_fingerprint           = "C4:AD:2D:F4:5E:47:B6:85:BC:09:DD:48:DF:4B:42:8D:E3:A0:10:09"
       settings.name_identifier_format         = "urn:oasis:names:tc:SAML:2.0:attrname-format:unspecified"
       # Optional for most SAML IdPs
       settings.authn_context = "urn:oasis:names:tc:SAML:2.0:ac:classes:PasswordProtectedTransport"
