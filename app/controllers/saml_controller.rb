3740d53583078f3852eed59d5ba31547384e5f8e
Edward Leung <EdwardLeung@localhost.(none)>
2012-12-05 14:19:57 -0500

    Made changes to saml settings

--- app/controllers/saml_controller.rb
+++ app/controllers/saml_controller.rb
@@ -23,7 +23,7 @@ class SamlController < ApplicationController
 
       settings.assertion_consumer_service_url = saml_consume_url(host: request.host)
       settings.issuer                         = "http://#{request.port == 80 ? request.host : request.host_with_port}"
-      settings.idp_sso_target_url             = "https://primedia.okta.com/app/template_saml/k18eco74OXEHHWDWRDMZ/sso/saml"
+      settings.idp_sso_target_url             = "https://primedia.okta.com/app/template_saml_2_0/k1qw8p4zXFYGZSRWDWQS/sso/saml"
       settings.idp_cert_fingerprint           = "C4:AD:2D:F4:5E:47:B6:85:BC:09:DD:48:DF:4B:42:8D:E3:A0:10:09"
       settings.name_identifier_format         = "urn:oasis:names:tc:SAML:2.0:attrname-format:unspecified"
       # Optional for most SAML IdPs
